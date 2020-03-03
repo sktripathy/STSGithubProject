c96f7037ace8068a773020704b1148c3f739eca5
debak <debak@DESKTOP-SG5TFN3>
2020-03-03 12:38:30 +0530

    first commit

---
+++ STSGithubProject/src/main/java/com/sagar/spring/STSGithubProject/App.java
@@ -0,0 +1,13 @@
+package com.sagar.spring.STSGithubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
